7c91bb21f6753273a83b69e9ddfb9d067802f65c
Victor Benarbia <jvmvik@gmail.com>
2014-12-10 14:16:48 +0000

    Migrate directory structure to npm module.

---
+++ nodek8055/examples/simple.js
@@ -0,0 +1,5 @@
+var k = require('../k8055').k8055;
+k.connect(0);
+k.setDigital(255);
+k.disconnect();
+
---
+++ nodek8055/k8055.js
@@ -0,0 +1,6 @@
+var k8055 = require('./build/Release/k8055');
+
+module.exports = {
+  k8055: k8055
+};
+
